3cf254c8a1b58d3afd0cb6ea372eb1781312259a
juanpuerto23 <juan2224511@correo.uis.edu.co>
2023-03-23 10:40:56 -0500

    Subiendo el modelo cliente

--- Fuente/Modelo/Cliente.java
+++ Fuente/Modelo/Cliente.java
@@ -1,5 +1,51 @@
-
 public class Cliente 
 {
+    public int presupuesto;
+    public String fechaIngreso;
+    public String fechaSalida;
+
+    public Cliente(String pFechaSalida, String pFechaIngreso, int pPresupuesto)
+    {
+        this.fechaIngreso = pFechaIngreso;
+        this.presupuesto = pPresupuesto;
+        this.fechaSalida = pFechaSalida;
+    }
+
+    public int getPresupuesto()
+    {
+        return this.presupuesto;
+    }
+
+    public void setPresupuesto(int pPresupuesto)
+    {
+        this.presupuesto = pPresupuesto;
+    }
+
+    public String getFechaIngreso()
+    {
+        return this.fechaIngreso;
+    }
+
+    public void setFechaIngreso(String pFechaIngreso)
+    {
+        this.fechaIngreso = pFechaIngreso;
+    }
+
+    public String getFechaSalida()
+    {
+        return this.fechaSalida;
+    }
+
+    public void setFechaSalida(String pFechaSalida)
+    {
+        this.fechaSalida = pFechaSalida;
+    }
+
+    public String toString()
+    {
+        return "";
+    }
+
+
 
 }
